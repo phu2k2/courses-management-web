520a654b621c36c7dbe2bb24827b37bbd958c15b
st-quangtran2 <quang.tran2@supremetech.vn>
2023-10-09 17:01:44 +0700

    create fake data and seeding for lesson

---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Lesson>
+ */
+class LessonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title(),
+            'lesson_duration' => fake()->randomFloat(2, 0.01, 300.99),
+            'topic_id' => fake()->randomNumber(),
+            'lesson_url' => fake()->unique()->url()
+        ];
+    }
+}
