b82c9b0c375f66326f81e791cb53f4de5a33709f
st-huyennguyen <huyen.nguyen@supremetech.vn>
2023-10-11 00:05:48 +0700

    fix RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,7 +9,7 @@ class RegisterController extends Controller
 {
     public function show(): View
     {
-        return view('user.auth.login');
+        return view('user.auth.register');
     }
 
     public function store(): RedirectResponse
