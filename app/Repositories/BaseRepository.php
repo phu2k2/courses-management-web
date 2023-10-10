b25af06754e0e4e960b0dbd83629f013efb87ba1
thanhvu <vu.tran@classmethod.vn>
2023-10-10 11:21:48 +0700

    Create base repo and service

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\RepositoryInterface;
+
+abstract class BaseRepository implements RepositoryInterface
+{
+    /**
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $model;
+
+    //construct
+    public function __construct()
+    {
+        $this->setModel();
+    }
+
+    /**
+     * Get the model instance for the repository.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    abstract public function getModel();
+
+    /**
+     * Set the model instance for the repository.
+     *
+     * @return void
+     */
+    public function setModel()
+    {
+        $this->model = app()->make(
+            $this->getModel()
+        );
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+    /**
+     * Find a record by its primary key.
+     *
+     * @param int $id The primary key value.
+     * @return \Illuminate\Database\Eloquent\Model|null The found model or null if not found.
+     */
+    public function find($id)
+    {
+        $result = $this->model->find($id);
+
+        return $result;
+    }
+
+    public function create($attributes = [])
+    {
+        return $this->model->create($attributes);
+    }
+    /**
+     * Update a record by its primary key.
+     *
+     * @param int $id The primary key value.
+     * @param array $attributes The data to update.
+     * @return bool Whether the update was successful or not.
+     */
+    public function update($id, $attributes = [])
+    {
+        $result = $this->find($id);
+        if ($result) {
+            $result->update($attributes);
+            return true;
+        }
+
+        return false;
+    }
+    /**
+     * Delete a record by its primary key.
+     *
+     * @param int $id The primary key value.
+     * @return bool True if the deletion was successful, false otherwise.
+     */
+    public function delete($id)
+    {
+        $result = $this->find($id);
+        if ($result) {
+            $result->delete();
+
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+interface RepositoryInterface
+{
+    /**
+     * Get all
+     * @return mixed
+     */
+    public function getAll();
+
+    /**
+     * Find a record by its primary key.
+     *
+     * @param int $id The primary key value.
+     * @return \Illuminate\Database\Eloquent\Model|null The found model or null if not found.
+     */
+    public function find($id);
+
+    /**
+     * Create
+     * @param array $attributes
+     * @return mixed
+     */
+    public function create($attributes = []);
+    /**
+     * Update a record by its primary key.
+     *
+     * @param int $id The primary key value.
+     * @param array $attributes The data to update.
+     * @return bool Whether the update was successful or not.
+     */
+    public function update($id, $attributes = []);
+
+    /**
+     * Delete a record by its primary key.
+     *
+     * @param int $id The primary key value.
+     * @return bool True if the deletion was successful, false otherwise.
+     */
+    public function delete($id);
+}
