f922a2e30effe225605bfcc72576c4b377913211
cmvn-phunguyen2 <phu.nguyen@classmethod.vn>
2023-10-10 15:10:25 +0700

    Finish Lesson Detail UI

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('assets/img/brand.svg') }}">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- Font -->
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
     <link
---
+++ resources/views/user/lesson/header.blade.php
@@ -0,0 +1,22 @@
+    <!-- NAVBAR
+    ================================================== -->
+    <header class="bg-portgore py-3">
+        <div class="px-5 px-lg-8 w-100">
+            <div class="d-md-flex align-items-center">
+                <!-- Brand -->
+                <a class="navbar-brand mb-2 mb-md-0" href="index.html">
+                    <img src="assets/img/brand-white.svg" class="navbar-brand-img" alt="...">
+                </a>
+
+                <!-- Lesson Title -->
+                <div class="mx-auto mb-5 mb-md-0">
+                    <h3 class="mb-0 line-clamp-2 text-white">Learn Figma: User Interface Design Essentials - UI/UX Design
+                    </h3>
+                </div>
+
+                <!-- Back to Course -->
+                <a href="course-single-v1.html"
+                    class="btn btn-sm btn-orange ms-md-6 px-6 mb-3 mb-md-0 flex-shrink-0">Back to Course</a>
+            </div>
+        </div>
+    </header>
