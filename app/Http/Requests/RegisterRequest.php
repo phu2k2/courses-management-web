275668c55c0987e5a94767c2d4c917724b1ec8fd
st-quangtran2 <quang.tran2@supremetech.vn>
2023-10-10 19:45:30 +0700

    create form request validation register

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => [
+                'required',
+                'string',
+                'regex:/^\S*$/u',
+                'max:255',
+                'unique:users.username'
+            ],
+            'email' => [
+                'required',
+                'email',
+                'max:50',
+                'unique:users,email'
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
+            ]
+        ];
+    }
+}
