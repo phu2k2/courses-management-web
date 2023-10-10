edf86cf080495a7408fdaa164bdc5f925fcfc173
phucchp <phuc.cao@supremetech.vn>
2023-10-10 16:04:57 +0700

    fix return type of getModel of baseRepository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\BaseRepository;
+use App\Repositories\Course\CourseRepository;
+use App\Repositories\Course\CourseRepositoryInterface;
+use App\Repositories\RepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            CourseRepositoryInterface::class,
+            CourseRepository::class
+        );
     }
 
     /**
