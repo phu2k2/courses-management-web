76837d69e00bcf00947ead58140804d110b3763f
phucchp <phuc.cao@supremetech.vn>
2023-10-11 13:38:03 +0700

    create helper

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!function_exists('convertToK')) {
+
+    /**
+     * @return string
+     */
+    function convertToK(int $number): string
+    {
+        if ($number < 1000) {
+            return (string)$number;
+        } elseif ($number < 1000000) {
+            return number_format($number / 1000, 1) . 'k';
+        } else {
+            return number_format($number / 1000000, 1) . 'M';
+        }
+    }
+}
