0c9fabaa1833bca30a0feb915b6c6b12736ae864
phucchp <phuc.cao@supremetech.vn>
2023-10-12 08:54:38 +0700

    add unique in category factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CategoryFactory extends Factory
 {
+    protected $model = Category::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +18,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->word,
+            'name' => fake()->unique->word,
         ];
     }
 }
