b5c74b6cd8dd7bd28a56bfc16b09bcfe8ccd869a
phucchp <phuc.cao@supremetech.vn>
2023-10-12 14:56:46 +0700

    remove id unused in CourseController

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -11,7 +11,7 @@ class CourseController extends Controller
         return view('course.index');
     }
 
-    public function show(int $id): View
+    public function show(): View
     {
         return view('course.show');
     }
