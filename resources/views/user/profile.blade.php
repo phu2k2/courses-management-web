5b00b56085ad0979994f0108751c00209c929f05
st-huyennguyen <huyen.nguyen@supremetech.vn>
2023-10-12 15:51:29 +0700

    add # into href and change check relation

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -27,9 +27,7 @@
                                             <div class="form-group">
                                                 <label for="inputUsername">Description</label>
                                                 <textarea rows="2" class="form-control" id="inputBio" name="description">
-                                                    @if (!empty($user->profile))
-                                                        {{ $user->profile->description }}
-                                                    @endif
+                                                        {{ $user->profile?->description }}
                                                 </textarea>
                                             </div>
                                         </div>
@@ -52,13 +50,13 @@
                                             <label for="inputFirstName">First name</label>
                                             <input type="text" class="form-control" id="inputFirstName"
                                                 placeholder="First name" name="first_name"
-                                                value="{{ !empty($user->profile) ? $user->profile->first_name : '' }}">
+                                                value="{{ $user->profile?->first_name }}">
                                         </div>
                                         <div class="form-group col-md-6">
                                             <label for="inputLastName">Last name</label>
                                             <input type="text" class="form-control" id="inputLastName"
                                                 placeholder="Last name" name="last_name"
-                                                value="{{ !empty($user->profile) ? $user->profile->last_name : '' }}">
+                                                value="{{ $user->profile?->last_name }}">
                                         </div>
                                     </div>
                                     <div class="form-group pb-3">
