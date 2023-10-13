5de900f1267faae37d989bfd08e7d307b0a05456
phucchp <phuc.cao@supremetech.vn>
2023-10-13 15:53:32 +0700

    remove comment

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -518,19 +518,6 @@
                 <nav class="mb-11" aria-label="Page navigationa">
                     <ul class="pagination justify-content-center">
                         {!! $courses->links('pagination::bootstrap-4') !!}
-                        {{-- <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true"><i class="fas fa-arrow-left"></i></span>
-                            </a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next">
-                                <span aria-hidden="true"><i class="fas fa-arrow-right"></i></span>
-                            </a>
-                        </li> --}}
                     </ul>
                 </nav>
             </div>
