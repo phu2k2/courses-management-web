04c3b06ba9f875d437fb752857cbc16e60748b1b
cmvn-phunguyen2 <phu.nguyen@classmethod.vn>
2023-10-13 17:13:37 +0700

    Fix CartRepositoryInterface

--- app/Repositories/Interfaces/CartRepositoryInterface.php
+++ app/Repositories/Interfaces/CartRepositoryInterface.php
@@ -9,5 +9,10 @@ use Illuminate\Database\Eloquent\Collection;
 interface CartRepositoryInterface extends RepositoryInterface
 {
     public function getCartByUser(int $id): Collection;
+    /**
+     * Store a newly created resource in storage.
+     * @param array $data
+     * @return \Illuminate\Database\Eloquent\Model
+     */
     public function addToCart($data): Model;
 }
