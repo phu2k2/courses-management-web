8e34f7be2163b3948df66d1af49881637979c005
phucchp <phuc.cao@supremetech.vn>
2023-10-13 08:57:18 +0700

    fix err space

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -12,7 +12,7 @@ if (!function_exists('convertToK')) {
         } elseif ($number < 1000000) {
             return number_format($number / 1000, 1) . 'k';
         }
-        
+
         return number_format($number / 1000000, 1) . 'M';
     }
 }
