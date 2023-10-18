8cba78536906cabd9c86994cf7e11b56dfe1dabe
st-huyennguyen <huyen.nguyen@supremetech.vn>
2023-10-18 08:59:11 +0700

    create comment UI with library cdn

---
+++ public/assets/js/comment.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function () {
+    $(".reply-comment").hide();
+    $(".btn-reply").on("click", function () {
+        var parentId = $(this).attr('data-parentId');
+        $(`.reply-comment.${parentId}`).toggle();
+    })
+
+    $(".reply-wrap").hide();
+    $(".show-reply").on("click", function () {
+        var parentId = $(this).attr('data-parentId');
+        $(`.reply-wrap.${parentId}`).show();
+        $(`.show-reply.${parentId}`).hide();
+    })
+
+    $(".hide-reply").on("click", function () {
+        var parentId = $(this).attr('data-parentId');
+        $(`.reply-wrap.${parentId}`).hide();
+        $(`.show-reply.${parentId}`).show();
+    })
+});
