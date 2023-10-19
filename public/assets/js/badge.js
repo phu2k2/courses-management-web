d0687a386f2d5f8f78b3474607bd72fb58626bf8
st-huyennguyen <huyen.nguyen@supremetech.vn>
2023-10-19 09:33:36 +0700

    add jquery show cart notification

--- public/assets/js/badge.js
+++ public/assets/js/badge.js
@@ -1,5 +1,8 @@
 $(document).ready(function (params) {
     var cartTotal = $("#badgeCart").text();
+    if (cartTotal == 0) {
+        $("#badgeCart").css("display", "none");
+    }
     if (cartTotal > 99) {
         $("#badgeCart").text('99+');
     }
