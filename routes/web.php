c911a92c0b5e331edda394f981068c2277a0376e
cmvn-phunguyen2 <phu.nguyen@classmethod.vn>
2023-10-19 13:46:16 +0700

    Finish Order UI

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,7 @@ Route::resource('carts', CartController::class)->only(['index', 'store', 'destro
 Route::prefix('courses')->name('courses.')->group(function () {
     Route::get('{courseId}/lessons/{lessonId}', [LessonController::class, 'show'])->name('lessons.show');
 });
+
+Route::get('/order', function () {
+    return view('order.index');
+})->name('order.index');
