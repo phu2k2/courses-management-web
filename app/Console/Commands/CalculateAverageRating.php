421a6871324f24165619903a3049e318de4b6bea
phucchp <phuc.cao@supremetech.vn>
2023-10-20 11:21:54 +0700

    create calculate-average-rating command

---
+++ app/Console/Commands/CalculateAverageRating.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Course;
+use Illuminate\Console\Command;
+
+class CalculateAverageRating extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:calculate-average-rating';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $coursesToUpdate = Course::with('reviews')->whereHas('reviews', function ($query) {
+            $query->whereNotNull('rating');
+        })->get();
+    
+        foreach ($coursesToUpdate as $course) {
+            $course->update(['average_rating' => $course->reviews->avg('rating')]);
+        }
+    }
+}
