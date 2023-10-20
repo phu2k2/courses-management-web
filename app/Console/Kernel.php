46eed54c76441896e6124425982e23903512acc4
phucchp <phuc.cao@supremetech.vn>
2023-10-20 11:23:51 +0700

    defined command schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CalculateAverageRating;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('app:calculate-average-rating')
+        ->everyThirtyMinutes();
     }
 
     /**
