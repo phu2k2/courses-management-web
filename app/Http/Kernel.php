2c6260dc3d9f256f8bd09498aa349ab9e4973a87
phucchp <phuc.cao@supremetech.vn>
2023-10-24 14:15:06 +0700

    rename middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkCourseOwnership' => \App\Http\Middleware\CheckCourseOwnership::class,
+        'verifyUserAccessCourse' => \App\Http\Middleware\VerifyUserAccessCourse::class,
     ];
 }
--- app/Http/Middleware/VerifyUserAccessCourse.php
+++ app/Http/Middleware/VerifyUserAccessCourse.php
@@ -7,7 +7,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CheckCourseOwnership
+class VerifyUserAccessCourse
 {
     /**
      * Handle an incoming request.
