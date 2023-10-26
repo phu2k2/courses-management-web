27b04fff2408804af73ac114235f17636d3e4c76
st-huyennguyen <huyen.nguyen@supremetech.vn>
2023-10-26 10:22:49 +0700

    update controller

--- app/Http/Controllers/Instructor/CourseController.php
+++ app/Http/Controllers/Instructor/CourseController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Instructor;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCourseRequest;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class CourseController extends Controller
 {
@@ -12,11 +14,17 @@ class CourseController extends Controller
      */
     public function create(): View
     {
+
         return view('instructor.course.create');
     }
 
     public function upload(): View
     {
         return view('instructor.course.upload');
+    }
+
+    public function store(): RedirectResponse
+    {
+        return redirect()->route('instructor.courses.upload');
     }
 }
