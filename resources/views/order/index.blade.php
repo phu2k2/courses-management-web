eb8245cb2606d62d4d55758f30ea86e105bb3b54
st-quangtran2 <quang.tran2@supremetech.vn>
2023-10-26 21:14:11 +0700

    increment student in course

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="bg-white woocommerce-order-received">
         <!-- SHOP ORDER COMPLETED
-                                    ================================================== -->
+                                                            ================================================== -->
         <div class="container py-8 py-lg-11">
             <div class="row">
                 <div class="col-xl-8 mx-xl-auto">
@@ -30,6 +30,9 @@
                                                 $total = 0;
                                             @endphp
                                             @foreach ($orders as $order)
+                                                @php
+                                                    $discountedPrice = data_get($order, 'course.discounted_price', 0);
+                                                @endphp
                                                 <tr class="woocommerce-table__line-item order_item">
                                                     <td class="woocommerce-table__product-name product-name">
                                                         <a href="#">{{ $order->course->title }}</a>
@@ -37,11 +40,11 @@
 
                                                     <td class="woocommerce-table__product-total product-total">
                                                         <span class="woocommerce-Price-amount amount"><span
-                                                                class="woocommerce-Price-currencySymbol">$</span>{{ number_format($order->course->discounted_price, 2) }}</span>
+                                                                class="woocommerce-Price-currencySymbol">$</span>{{ number_format($discountedPrice, 2) }}</span>
                                                     </td>
                                                 </tr>
                                                 @php
-                                                    $total += number_format($order->course->discounted_price, 2);
+                                                    $total += number_format($discountedPrice, 2);
                                                 @endphp
                                             @endforeach
                                         </tbody>
